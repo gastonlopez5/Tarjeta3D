93d78003a37ed8b73d29c27fa6a1b26c5e571347
gaston.lopez <gaston.lopez@BC-SL30>
2021-07-20 16:00:44 -0300

    Agregar archivos de proyecto.

---
+++ Tarjeta3D/Pages/CoolEffect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tarjeta3D.Pages
+{
+    public partial class CoolEffect
+    {
+    }
+}
